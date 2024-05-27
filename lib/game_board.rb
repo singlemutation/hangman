e7779ca863cf9719ba0ce038fc29ba0fc4bae4e2
singlemutation <164826575+singlemutation@users.noreply.github.com>
2024-05-26 21:44:08 -0400

    Make game board (blanks) from word chosen

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -5,6 +5,10 @@ require_relative 'word_picker'
 # Turn word into game board
 class GameBoard
   def initialize
+    @word = WordPicker.new.pick_word
+    p @word
+    @masked_word = letters_to_blanks(@word)
+    p @masked_word
   end
 
   def letters_to_blanks(word)
--- lib/word_picker.rb
+++ lib/word_picker.rb
@@ -2,20 +2,14 @@
 
 # Open the txt file and pick a random word that is the right length
 class WordPicker
-  def initialize
-    @word = pick_word
-  end
-
   attr_reader :word
 
   def pick_word
-    words = File.readlines('google-10000-english-no-swears.txt').map(&:chomp)
+    words = File.read('google-10000-english-no-swears.txt').split
     game_words = []
     words.each do |line|
       game_words.push(line) if line.length > 4 && line.length < 13
     end
-    game_words.sample(1)
+    game_words.sample.to_s
   end
 end
-
-@new_word = WordPicker.new
